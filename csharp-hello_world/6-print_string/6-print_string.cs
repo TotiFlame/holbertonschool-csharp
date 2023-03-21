a773ba71129e47563da3494b9f215c873584c0ff
TotiFlame <santifer4321@gmail.com>
2023-03-21 12:46:08 -0300

    Task 6

---
+++ csharp-hello_world/6-print_string/6-print_string.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+class Program
+{
+	static void Main(string[] args)
+	{
+		string str = "Holberton School";
+		Console.Write("{0}", str);
+		Console.Write("{0}", str);
+		Console.WriteLine("{0}", str);
+		Console.WriteLine("{0}", str.Substring(0, 9));
+        }
+}
