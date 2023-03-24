f7c57543cd88d47ceb6b918b5b510523fc7d6223
TotiFlame <santifer4321@gmail.com>
2023-03-24 17:26:55 -0300

    Task 6

---
+++ csharp-ifelse_loops_methods/6-print_comb2/6-print_comb2.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _6_print_comb2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i, j;
+            for (i = 0; i < 9; i++)
+            {
+                for (j = i + 1; j < 10; j++)
+                {
+                    if (i == 8 && j == 9)
+                    {
+                        Console.WriteLine("89");
+                        return;
+                    }
+                    Console.Write("{0}{1}, ", i, j);
+                }
+            }
+        }
+    }
+}
