0cf76ce285a2e3223214d9a9f14daf655d61846e
TotiFlame <santifer4321@gmail.com>
2023-03-24 16:12:25 -0300

    Task 3 fix

---
+++ csharp-ifelse_loops_methods/3-print_alphabt/3-print_alphabt.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int i;
+        for(i = 97; i < 123; i++) {
+            if (i != 101 && i != 113)
+                Console.Write("{0}", Convert.ToChar(i));
+        }
+    }
+}
