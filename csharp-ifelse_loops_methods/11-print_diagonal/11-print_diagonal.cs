85dbb35e5f1c48f216ed354d3571a0665e941e1f
TotiFlame <santifer4321@gmail.com>
2023-03-28 00:10:09 -0300

    Task 11

---
+++ csharp-ifelse_loops_methods/11-print_diagonal/11-print_diagonal.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Line
+{
+    public static void PrintDiagonal(int length)
+    {
+        if(length == 0 || length < 0) {
+            Console.WriteLine();
+            return;
+        }
+
+        int j, i;
+        for (i = 0; i < length; i++)
+        {
+            Console.WriteLine("\\");
+            for (j = 0; j <= i; j++)
+            {
+                Console.Write(" ");
+                // Console.Write("spaces: {0}", j);
+            }
+        }
+        Console.WriteLine();
+    }
+}
