68e8996cdffaf2deb1ee5d691977204a235d793d
TotiFlame <santifer4321@gmail.com>
2023-03-28 00:27:07 -0300

    Task 12

---
+++ csharp-ifelse_loops_methods/12-fizzbuzz/12-fizzbuzz.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        for (int i = 1; i <= 100; i++)
+        {
+            if ((i % 3 == 0) && (i % 5 == 0))
+            {
+                Console.Write("FizzBuzz ");
+                continue;
+            } else if (i % 3 == 0) {
+                Console.Write("Fizz ");
+                continue;
+            } else if (i % 5 == 0) {
+                Console.Write("Buzz ");
+                continue;
+            }
+            Console.Write("{0} ", i);
+        }
+    }
+}
