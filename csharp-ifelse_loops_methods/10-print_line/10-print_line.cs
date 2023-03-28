64060059e8f7b0f79eeb83d3a0a7d9cf5baaadd8
TotiFlame <santifer4321@gmail.com>
2023-03-27 23:47:00 -0300

    Task 10

---
+++ csharp-ifelse_loops_methods/10-print_line/10-print_line.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Line
+{
+    public static void PrintLine(int length)
+    {
+        int i;
+
+        if (length == 0) {
+            Console.WriteLine();
+            return;
+        }
+
+        for(i = 0; i < length; i++) {
+            Console.Write("_");
+        }
+        Console.WriteLine();
+    }
+}
