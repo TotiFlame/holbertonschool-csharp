b4e9f4739925ae2f0b7e505ca1a525a7f983d5cb
TotiFlame <santifer4321@gmail.com>
2023-03-27 23:26:21 -0300

    Task 8

---
+++ csharp-ifelse_loops_methods/8-print_last_digit/8-print_last_digit.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class Number
+{
+    public static int PrintLastDigit(int number)
+    {
+        int last_digit = number % 10;
+        
+        if(last_digit < 0) {
+            last_digit = last_digit * -1;
+        }
+
+        Console.Write("{0}", last_digit);
+        return(last_digit);
+    }
+}
