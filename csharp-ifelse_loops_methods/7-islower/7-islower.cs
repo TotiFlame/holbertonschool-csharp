5a53be15cd6b6ff7d2136675affd5247119291ff
TotiFlame <santifer4321@gmail.com>
2023-03-27 23:06:22 -0300

    Task 7

---
+++ csharp-ifelse_loops_methods/7-islower/7-islower.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+class Character
+{
+    public static bool IsLower(char c)
+    {
+        int ch = Convert.ToInt32(c);
+        if (ch > 96 && ch < 123) {
+            return (true);
+        } else
+            return (false);
+    }
+}
