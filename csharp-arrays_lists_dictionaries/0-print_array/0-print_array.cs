23c571a48ec90c99669f176624d38da47ab9f3fe
TotiFlame <santifer4321@gmail.com>
2023-03-29 16:36:56 -0300

    Task 0

---
+++ csharp-arrays_lists_dictionaries/0-print_array/0-print_array.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Array
+{
+    public static int[] CreatePrint(int size)
+    {
+        if (size < 0)
+        {
+            Console.WriteLine("Size cannot be negative");
+            return(null);
+        }
+
+        int[] new_array = new int[size];
+        int i;
+
+        for (i = 0; i < size; i++)
+        {
+            new_array[i] = i;
+            Console.Write("{0} ", new_array[i]);
+
+        }
+        Console.WriteLine();
+        return(new_array);
+    }
+}
