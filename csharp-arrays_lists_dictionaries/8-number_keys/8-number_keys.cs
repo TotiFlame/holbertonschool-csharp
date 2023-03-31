5d3231f3be46100b5b76a3d08b13947cb2553d92
TotiFlame <santifer4321@gmail.com>
2023-03-31 19:55:05 -0300

    Task 8

---
+++ csharp-arrays_lists_dictionaries/8-number_keys/8-number_keys.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Dictionary
+{
+    public static int NumberOfKeys(Dictionary<string, string> myDict)
+    {
+        if (myDict == null) {
+            return -1;
+        }
+        int cont = 0;
+
+        foreach (KeyValuePair<string, string> dict in myDict)
+        {
+            cont++;
+        }
+        return cont;
+    }
+}
