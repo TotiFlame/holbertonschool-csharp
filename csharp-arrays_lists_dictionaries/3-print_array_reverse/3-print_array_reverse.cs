25b38e75607cc2f7cb8a9b30c17652087963ffce
TotiFlame <santifer4321@gmail.com>
2023-03-31 10:49:22 -0300

    3

--- csharp-arrays_lists_dictionaries/3-print_array_reverse/3-print_array_reverse.cs
+++ csharp-arrays_lists_dictionaries/3-print_array_reverse/3-print_array_reverse.cs
@@ -9,13 +9,13 @@ class Array
         if (array == null || array.Length == 0)
         {
             Console.WriteLine();
-            return;
-        }
-        for (array_length = array.Length - 1; array_length >= 0; --array_length) {
-            if (array_length == 0) {
-                Console.Write("{0}", array[array_length]);
-            } else {
-                Console.Write("{0} ", array[array_length]);
+        } else {
+            for (array_length = array.Length - 1; array_length >= 0; --array_length) {
+                if (array_length == 0) {
+                    Console.Write("{0}", array[array_length]);
+                } else {
+                    Console.Write("{0} ", array[array_length]);
+                }
             }
         }
     }
