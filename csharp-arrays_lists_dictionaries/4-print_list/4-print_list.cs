ab3e70b7165257b8a7bb7d524a738c29f82b1fe2
TotiFlame <santifer4321@gmail.com>
2023-03-31 14:49:23 -0300

    Task 4

---
+++ csharp-arrays_lists_dictionaries/4-print_list/4-print_list.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class List
+{
+    public static List<int> CreatePrint(int size)
+    {
+        if (size < 0) {
+            Console.WriteLine("Size cannot be negative");
+            return(null);
+        }
+
+        List<int> numbers = new List<int>();
+        while (i < size) {
+        {
+            numbers.Add(i);
+            i++;
+        }
+        Console.WriteLine("{0}", numbers);
+        return(numbers);
+        }
+    }
+}
