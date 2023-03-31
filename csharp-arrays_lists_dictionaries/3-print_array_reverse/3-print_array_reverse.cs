a748aea21fcc37bed1e0a5188a8865b9f47ac12d
TotiFlame <santifer4321@gmail.com>
2023-03-31 10:40:39 -0300

    Task 3

--- csharp-arrays_lists_dictionaries/3-print_array_reverse/3-print_array_reverse.cs
+++ csharp-arrays_lists_dictionaries/3-print_array_reverse/3-print_array_reverse.cs
@@ -12,9 +12,11 @@ class Array
             return;
         }
         for (array_length = array.Length - 1; array_length >= 0; --array_length) {
-            Console.Write("{0} ", array[array_length]);
+            if (array_length == 0) {
+                Console.Write("{0}", array[array_length]);
+            } else {
+                Console.Write("{0} ", array[array_length]);
+            }
         }
-        Console.WriteLine();
-        return;
     }
 }
