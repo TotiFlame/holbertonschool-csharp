f0bcffbe998acbf458da75b993e07840b3bf34ba
TotiFlame <santifer4321@gmail.com>
2023-03-31 11:20:30 -0300

    Task 2

--- csharp-arrays_lists_dictionaries/2-replace_element/2-replace_element.cs
+++ csharp-arrays_lists_dictionaries/2-replace_element/2-replace_element.cs
@@ -5,7 +5,7 @@ class Array
 {
     public static int[] ReplaceElement(int[] array, int index, int n)
     {
-        if (index > array.Length || index < 0)
+        if (index >= array.Length || index < 0)
         {
             Console.WriteLine("Index out of range");
             return(array);
