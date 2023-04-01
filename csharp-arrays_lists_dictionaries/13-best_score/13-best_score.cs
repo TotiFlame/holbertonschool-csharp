eeff4eb67e93ddd4d93068418219f5f7a4463936
TotiFlame <santifer4321@gmail.com>
2023-04-01 10:17:44 -0300

    Task 14

---
+++ csharp-arrays_lists_dictionaries/13-best_score/13-best_score.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _13_best_score
+{
+    class Program
+    {
+        public static string BestScore(Dictionary<string, int> myList)
+        {
+            int maxScore = 0;
+            string student = "";
+            foreach (KeyValuePair<string, int> item in myList)
+            {
+                if (maxScore < item.Value)
+                {
+                    maxScore = item.Value;
+                    student = item.Key;
+                }
+            }
+            return student;
+        }
+    }
+}
---
+++ csharp-arrays_lists_dictionaries/14-rectangular_array/14-rectangular_array.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[,] result = { {0, 0, 0, 0, 0}, {0, 0, 0, 0, 0}, {0, 0, 1, 0, 0}, {0, 0, 0, 0, 0}, {0, 0, 0, 0, 0} };
+        int arr_length = result.Length;
+        for (int i = 0; i < 5; i++)
+        {
+            for (int j = 0; j < 5; j++) {
+                if (j == 4)
+                {
+                    Console.Write(result[i,j]);
+                    continue;
+                }
+                Console.Write(result[i,j] + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
+
