5d5e71ffca9bfd903f0130fdc3b3a10477dd1b4e
TotiFlame <santifer4321@gmail.com>
2023-03-31 21:11:58 -0300

    Task 12

---
+++ csharp-arrays_lists_dictionaries/12-print_sorted_dictionary/12-print_sorted_dictionary.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Dictionary
+{
+    public static void PrintSorted(Dictionary<string, string> myDict)
+    {
+        var sortedDict = myDict.OrderBy(x => x.Key);
+        foreach (var each in sortedDict)
+        {
+            Console.WriteLine(each.Key + ": " + each.Value);
+        }
+    }
+}
