6bccb4b23626652283f0a427d3983247101ef9c3
TotiFlame <santifer4321@gmail.com>
2023-04-05 16:09:53 -0300

    Task 3

---
+++ csharp-hashset_stack_queue_linkedlist/3-stack_push_pop/3-stack_push_pop.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+class MyStack
+{
+    public static Stack<string> Info(Stack<string> aStack, string newItem, string search)
+    {
+        string top = aStack.Peek();
+        bool contains = aStack.Contains(search);
+
+        Console.WriteLine("Number of items: {0}", aStack.Count);
+        Console.WriteLine("Top Item: {0}", top);
+        Console.WriteLine("Stack contains \"{0}\": {1}", search, contains);
+        
+        while (aStack.Contains(search))
+        {
+            aStack.Pop();
+        }
+        aStack.Push(newItem);
+        return aStack;
+    }
+}
