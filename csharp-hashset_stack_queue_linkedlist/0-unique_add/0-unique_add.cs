1d57f943cf6ac18c06e6499841bd0fa137adfe56
TotiFlame <santifer4321@gmail.com>
2023-04-05 14:53:21 -0300

    Task 0

---
+++ csharp-hashset_stack_queue_linkedlist/0-unique_add/0-unique_add.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class List
+{
+    public static int Sum(List<int> myList)
+    {
+        List<int> newList = myList.Distinct().ToList();
+        int sum = 0;
+
+        foreach (var item in newList)
+        {
+            sum += item;
+        }
+        return sum;
+    }
+}
