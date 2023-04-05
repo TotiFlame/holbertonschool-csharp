399c77a4b62dfc4001472bc6deeb1b63a08a5cc9
TotiFlame <santifer4321@gmail.com>
2023-04-05 15:05:49 -0300

    Task 1

---
+++ csharp-hashset_stack_queue_linkedlist/1-common_elements/1-common_elements.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static List<int> CommonElements(List<int> list1, List<int> list2)
+    {
+        List<int> result = new List<int>();
+        int i, j;
+
+        for (i = 0; i < list1.Count; i++)
+        {
+            for (j = 0; j < list2.Count; j++)
+            {
+                if (list1[i] == list2[j]) {
+                    result.Add(list1[i]);
+                }
+            }
+        }
+        return result;
+    }
+}
