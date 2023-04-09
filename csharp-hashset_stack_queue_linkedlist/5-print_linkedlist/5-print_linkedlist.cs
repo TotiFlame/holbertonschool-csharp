ff4ff86e570c8a281eb877a53c24f5dd0e0feb94
TotiFlame <santifer4321@gmail.com>
2023-04-09 01:53:56 -0300

    Task 5

---
+++ csharp-hashset_stack_queue_linkedlist/5-print_linkedlist/5-print_linkedlist.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static LinkedList<int> CreatePrint(int size)
+    {
+        if (size < 0)
+        {
+            LinkedList<int> newLinkedList = new LinkedList<int>();
+            return newLinkedList;
+        }
+        LinkedList<int> myLinkedList = new LinkedList<int>();
+        int i;
+        for (i = 0; i < size; i++)
+        {
+            myLinkedList.AddLast(i);
+            Console.WriteLine("{0}", i);
+        }
+        return myLinkedList;
+    }
+}
