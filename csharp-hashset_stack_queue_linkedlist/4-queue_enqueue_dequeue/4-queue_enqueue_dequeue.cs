dbcefbcd25e27996f8fe1e5779a693b63b8ff4c6
TotiFlame <santifer4321@gmail.com>
2023-04-08 23:45:52 -0300

    Task 4

---
+++ csharp-hashset_stack_queue_linkedlist/4-queue_enqueue_dequeue/4-queue_enqueue_dequeue.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+class MyQueue
+{
+    public static Queue<string> Info(Queue<string> aQueue, string newItem, string search)
+    {
+        if (aQueue == null)
+        {
+            Console.WriteLine("Queue is empty");
+        }
+        string top = aQueue.Peek();
+        bool found = aQueue.Contains(search);
+
+        Console.WriteLine("Number of items: {0}", aQueue.Count);
+        Console.WriteLine("First item: {0}", top);
+        Console.WriteLine("Queue contains \"{0}\": {1}", search, found);
+        while (aQueue.Contains(search))
+        {
+            aQueue.Dequeue();
+        }
+        aQueue.Enqueue(newItem);
+        return aQueue;
+    }
+}
