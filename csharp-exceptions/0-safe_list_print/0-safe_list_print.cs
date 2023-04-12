630f759c12f8cb51826cb9907d94fffe0081769a
TotiFlame <santifer4321@gmail.com>
2023-04-12 15:30:11 -0300

    Task 0

---
+++ csharp-exceptions/0-safe_list_print/0-safe_list_print.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static int SafePrint(List<int> myList, int n)
+    {
+        try
+        {
+            int count = 0;
+            if (n > myList.Count) {
+                n = myList.Count;
+            }
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine(myList[i]);
+                count++;
+            }
+            return count;
+        }
+        catch (System.Exception)
+        {
+            throw;
+        }
+    }
+}
