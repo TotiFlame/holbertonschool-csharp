f243f32c54691ac245f103f93f05808a15365d7d
TotiFlame <santifer4321@gmail.com>
2023-04-13 16:24:20 -0300

    Task 2

---
+++ csharp-exceptions/2-divide_lists/2-divide_lists.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static List<int> Divide(List<int> list1, List<int> list2, int listLength)
+    {
+        List<int> result = new List<int>();
+        for (int i = 0; i < listLength; i++)    
+        {
+            try
+            {
+                int dividend = list1[i];
+                int divisor = list2[i];
+
+                if (divisor == 0)
+                {
+                    Console.WriteLine("Cannot divide by zero");
+                    result.Add(0);
+                }
+                else
+                {
+                    result.Add(dividend/divisor);
+                }
+            }
+            catch(System.Exception)
+            {
+                Console.WriteLine("Out of range");
+            }
+        }
+    return result;
+    }
+}
