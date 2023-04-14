1348fc0f04560af5fd8e870c2a963668b7e04ae1
TotiFlame <santifer4321@gmail.com>
2023-04-14 10:01:15 -0300

    Task 1

---
+++ csharp-structs_enums/1-dog/1-dog.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+public enum Rating {
+    Good = 0,
+    Great = 1,
+    Excellent = 2
+}
+
+public struct Dog {
+    public string name;
+    public float age;
+    public string owner;
+    public Rating rating;
+}
