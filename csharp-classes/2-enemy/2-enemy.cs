24b768854dcdef597353ea124c429d239968eb10
TotiFlame <santifer4321@gmail.com>
2023-04-21 00:41:22 -0300

    Task 2

---
+++ csharp-classes/2-enemy/2-enemy.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Enemies
+{
+    /// <summary> Public class that defines a Zombie </summary>
+    public class Zombie
+    {
+        /// <summary> Public int named health</summary>
+        public int health;
+
+        /// <summary> Public constructor Zombie without parameters </summary>
+        public Zombie()
+        {
+            health = 0;
+        }
+        /// <summary> Public constructor Zombie with parameters </summary>
+        public Zombie(int value)
+        {
+            if (value < 0)
+            {
+                throw new System.ArgumentException("Health must be greater than or equal to 0");
+            }
+            health = value;
+        }
+    }
+}
