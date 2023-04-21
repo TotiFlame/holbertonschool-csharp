1c4b5f0246e5798c4324afb06ce222cd790b0eba
TotiFlame <santifer4321@gmail.com>
2023-04-21 00:23:54 -0300

    Task 1

--- csharp-classes/1-enemy/1-enemy.cs
+++ csharp-classes/1-enemy/1-enemy.cs
@@ -2,11 +2,13 @@
 
 namespace Enemies
 {
-    /// <summary> Class Zombie, whit healt field initialized in 0 </summary>
+    /// <summary> Public class that defines a Zombie </summary>
     public class Zombie
     {
+        /// <summary> Public int named health</summary>
         public int health;
 
+        /// <summary> Public constructor Zombie </summary>
         public Zombie()
         {
             health = 0;
