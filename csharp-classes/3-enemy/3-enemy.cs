bb35224ba2a7e6a98cea46d6cb1be52eead58a3d
TotiFlame <santifer4321@gmail.com>
2023-04-21 01:04:34 -0300

    Task 3

---
+++ csharp-classes/3-enemy/3-enemy.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Enemies
+{
+    /// <summary> Public class that defines a Zombie </summary>
+    public class Zombie
+    {
+        /// <summary> Public int named health</summary>
+        private int health;
+
+        /// <summary> Public constructor Zombie without parameters </summary>
+        public Zombie()
+        {
+            health = 0;
+        }
+        /// <summary> Public constructor Zombie with parameters </summary>
+        public Zombie(int value)
+        {
+            if (value < 0)
+            {
+                throw new System.ArgumentException("Health must be greater than or equal to 0");
+            }
+            health = value;
+        }
+        /// <summary> Public health variable getter</summary>
+        public int GetHealth()
+        {
+            return health;
+        }
+    }
+}
