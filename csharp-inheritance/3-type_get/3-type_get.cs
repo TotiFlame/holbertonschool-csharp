8920afab9c214208fac2f35f94c6601e8c2d563d
TotiFlame <santifer4321@gmail.com>
2023-04-27 18:34:58 -0300

    Task 3

---
+++ csharp-inheritance/3-type_get/3-type_get.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Reflection;
+
+class Obj
+{
+    /// <summary> Method that prints properties and methods of an object.</summary>
+    public static void Print(object myObj)
+    {
+        PropertyInfo[] properties = myObj.GetType().GetProperties();
+        foreach (PropertyInfo prop in properties)
+        {
+            Console.WriteLine(prop.Name);
+        }
+        MethodInfo[] methods = myObj.GetType().GetMethods();
+        foreach (MethodInfo meth in methods)
+        {
+            Console.WriteLine(meth.Name);
+        }
+    }
+}
