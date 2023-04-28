afa700386b669fc6bd0459f0f4d04866ad36c60b
TotiFlame <santifer4321@gmail.com>
2023-04-28 10:36:21 -0300

    Task 6 fix

--- csharp-inheritance/6-shape/6-shape.cs
+++ csharp-inheritance/6-shape/6-shape.cs
@@ -43,7 +43,7 @@ class Rectangle : Shape
         {
             if (value < 0)
             {
-                throw new ArgumentException("height must be greater than or equal to 0");
+                throw new ArgumentException("Height must be greater than or equal to 0");
             }
             else
             {
