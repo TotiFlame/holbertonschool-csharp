5975e2333ab9af6d898f1ef98b0b0432cc1b04d9
TotiFlame <santifer4321@gmail.com>
2023-04-28 10:33:32 -0300

    Task 6

---
+++ csharp-inheritance/6-shape/6-shape.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+class Shape
+{
+    /// <summary> public area method </summary>
+    public virtual int Area()
+    {
+        throw new NotImplementedException("Area() is not implemented");
+    }
+}
+
+class Rectangle : Shape
+{
+    private int width;
+    private int height;
+
+    public int Width
+    {
+        get 
+        {
+            return width;
+        }
+        set
+        {
+            if (value < 0)
+            {
+                throw new ArgumentException("Width must be greater than or equal to 0");
+            }
+            else
+            {
+                width = value;
+            }
+        } 
+    }
+
+    public int Height
+    {
+        get 
+        {
+            return height;
+        }
+        set
+        {
+            if (value < 0)
+            {
+                throw new ArgumentException("height must be greater than or equal to 0");
+            }
+            else
+            {
+                height = value;
+            }
+        } 
+    }
+}
